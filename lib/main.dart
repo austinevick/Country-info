a2c0a2b8b4004b1ba9886646945c6045d3219df2
Augustine Victor <augustinevickky@gmail.com>
2020-12-23 18:56:56 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+import 'model/countries.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Future<List<Countries>> fetch() async {
+    final url = 'https://restcountries.eu/rest/v2/all';
+    List<Countries> countries = [];
+    final response = await get(url);
+    if (response.statusCode == 200) {
+      final result = jsonDecode(response.body);
+      List<dynamic> countryList = result;
+      print(countryList);
+      countryList
+          .forEach((element) => countries.add(Countries.fromMap(element)));
+      return countries;
+    }
+    throw Exception('Unable to fetch data');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder<List<Countries>>(
+        future: fetch(),
+        builder: (context, snapshot) => ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (ctx, i) => Center(
+              child: ListTile(
+                  title: Column(
+                      children: snapshot.data[i].coordinate
+                          .map((e) => Text(e.toString()))
+                          .toList()),
+                  subtitle: Column(
+                    children: snapshot.data[i].languages
+                        .map((e) => Text(e.name))
+                        .toList(),
+                  ),
+                  leading: SvgPicture.network(
+                      "https://restcountries.eu/data/col.svg"))),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetch,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/model/countries.dart
@@ -0,0 +1,67 @@
+class Countries {
+  final String name;
+  final String capital;
+  final int population;
+  final List<double> coordinate;
+  final List<Currencies> currencies;
+  final List<Language> languages;
+  final String flag;
+  Countries({
+    this.name,
+    this.capital,
+    this.population,
+    this.coordinate,
+    this.currencies,
+    this.languages,
+    this.flag,
+  });
+  factory Countries.fromMap(Map<String, dynamic> map) {
+    List<Currencies> currencyList = [];
+    List<Language> languageList = [];
+    List<double> coordinateList = [];
+    map['latlng'].forEach((coordinate) => coordinateList.add(coordinate));
+    map['currencies']
+        .forEach((currency) => currencyList.add(Currencies.fromMap(currency)));
+    map['languages']
+        .forEach((lang) => languageList.add(Language.fromMap(lang)));
+    return Countries(
+        name: map['name'],
+        capital: map['capital'],
+        population: map['population'],
+        coordinate: coordinateList,
+        currencies: currencyList,
+        languages: languageList,
+        flag: map['flag']);
+  }
+}
+
+class Currencies {
+  final String name;
+  final String symbol;
+  Currencies({this.name, this.symbol});
+
+  factory Currencies.fromMap(Map<String, dynamic> map) {
+    return Currencies(name: map['name'], symbol: map['symbol']);
+  }
+
+  @override
+  String toString() => 'Currencies(name: $name, symbol: $symbol)';
+}
+
+class Language {
+  final String name;
+  Language({
+    this.name,
+  });
+
+  @override
+  String toString() => 'Languages(name: $name)';
+
+  factory Language.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return Language(
+      name: map['name'],
+    );
+  }
+}
